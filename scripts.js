31db843cc7942208d5678f731a620cef6825c972
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-07-12 18:34:23 +0500

    Add Sticky Header to All Blogs Page

--- scripts.js
+++ scripts.js
@@ -12,6 +12,24 @@ const blogData = [
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer commodo, metus in tincidunt fringilla, nisl nunc commodo nisi, in cursus ligula neque non arcu.'
   },
   {
+    id: '1',
+    title: 'Blog Title 1',
+    date: 'July 1, 2023',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer commodo, metus in tincidunt fringilla, nisl nunc commodo nisi, in cursus ligula neque non arcu.'
+  },
+  {
+    id: '1',
+    title: 'Blog Title 1',
+    date: 'July 1, 2023',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer commodo, metus in tincidunt fringilla, nisl nunc commodo nisi, in cursus ligula neque non arcu.'
+  },
+  {
+    id: '1',
+    title: 'Blog Title 1',
+    date: 'July 1, 2023',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer commodo, metus in tincidunt fringilla, nisl nunc commodo nisi, in cursus ligula neque non arcu.'
+  },
+  {
     id: '2',
     title: 'Blog Title 2',
     date: 'July 5, 2023',
@@ -67,13 +85,17 @@ blogData.forEach((blog) => {
 });
 
 window.addEventListener('scroll', function() {
-  const navbar = document.getElementById('navbar');
-  const navbarHeight = navbar.offsetHeight;
+  var navbar = document.getElementById('navbar');
+  var image = navbar.querySelector('img');
+  var heading = navbar.querySelector('h1');
+  var offset = navbar.offsetTop;
 
-  if (window.scrollY >= navbarHeight) {
+  if (window.scrollY > offset + 50) {
     navbar.classList.add('sticky');
   } else {
     navbar.classList.remove('sticky');
+    image.style.height = ''; // Reset the height to its original value
+    heading.style.fontSize = ''; // Reset the font size to its original value
   }
 });
 
